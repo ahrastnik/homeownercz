2da19aa031ab80dcbccc546cd73a988ccc63f8fd
Adam Hrastnik <adam.hrastnik@gmail.com>
2023-08-27 00:37:40 +0200

    Formatted project with black.

--- backend/crawler/spiders/properties.py
+++ backend/crawler/spiders/properties.py
@@ -9,12 +9,14 @@ from crawler.items import PropertyItem
 class PropertySpider(scrapy.Spider):
     name = 'property'
 
-    def __init__(self,
-                 name=None,
-                 scrape_limit=500,
-                 domain="https://www.sreality.cz",
-                 rel_url="/en/search/for-sale/apartments/",
-                 **kwargs):
+    def __init__(
+        self,
+        name=None,
+        scrape_limit=500,
+        domain="https://www.sreality.cz",
+        rel_url="/en/search/for-sale/apartments/",
+        **kwargs
+    ):
         super().__init__(name, **kwargs)
         self.scrape_limit = int(scrape_limit)
         self.REQUEST_META = dict(
@@ -30,23 +32,29 @@ class PropertySpider(scrapy.Spider):
 
     def start_requests(self):
         yield scrapy.Request(self.url, meta=self.REQUEST_META)
-    
+
     def has_reached_scrape_limit(self):
-        scrape_count = self.crawler.stats.get_value('properties/scraped_count', default=0)
+        scrape_count = self.crawler.stats.get_value(
+            'properties/scraped_count', default=0
+        )
         return scrape_count >= self.scrape_limit
 
     def parse(self, response):
         for prop in response.css('div.property'):
             if self.has_reached_scrape_limit():
                 return
-            
-            property_item = PropertyItem({
-                'name': prop.css('span.name::text').get(),
-                'locality': prop.css('span.locality::text').get(),
-                'price': prop.css('span.norm-price::text').get(),
-                'url': urljoin(self.domain, prop.css('a.title::attr(href)').get()),
-                'image_url': prop.css('a._2vc3VMce92XEJFrv8_jaeN img::attr(src)').get(),
-            })
+
+            property_item = PropertyItem(
+                {
+                    'name': prop.css('span.name::text').get(),
+                    'locality': prop.css('span.locality::text').get(),
+                    'price': prop.css('span.norm-price::text').get(),
+                    'url': urljoin(self.domain, prop.css('a.title::attr(href)').get()),
+                    'image_url': prop.css(
+                        'a._2vc3VMce92XEJFrv8_jaeN img::attr(src)'
+                    ).get(),
+                }
+            )
             self.crawler.stats.inc_value('properties/scraped_count')
             yield property_item
 
@@ -54,13 +62,9 @@ class PropertySpider(scrapy.Spider):
         if not next_page or self.has_reached_scrape_limit():
             return
         next_url = urljoin(self.domain, next_page)  # Make an absolute URL
-        
-        yield response.follow(
-            next_url,
-            callback=self.parse,
-            meta=self.REQUEST_META
-        )
-    
+
+        yield response.follow(next_url, callback=self.parse, meta=self.REQUEST_META)
+
     async def error_callback(self, failure):
         page = failure.request.meta["playwright_page"]
         await page.close()
--- backend/homeownercz/models.py
+++ backend/homeownercz/models.py
@@ -2,8 +2,12 @@ from django.db import models
 
 
 class Property(models.Model):
-    url = models.SlugField(primary_key=True, max_length=256, help_text='Link to the property listing')
+    url = models.SlugField(
+        primary_key=True, max_length=256, help_text='Link to the property listing'
+    )
     name = models.CharField(max_length=128, help_text='Name of the property')
     price = models.CharField(max_length=128, help_text='Price of the property')
     locality = models.CharField(max_length=128, help_text='Location of the property')
-    image_url = models.SlugField(max_length=512, help_text='Link to the image of the property')
+    image_url = models.SlugField(
+        max_length=512, help_text='Link to the image of the property'
+    )
